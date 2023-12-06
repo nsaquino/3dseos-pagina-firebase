1da01fd8696d75d225dca810a28540f59b16c616
Nico Aquino <aquino_nicolas@hotmail.com>
2023-12-06 12:03:14 -0300

    Reordena y prepara para firebase

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATciwvN_VtwKcYaKu9uZag73Ib_eKn1Jk",
+  authDomain: "test-3dseos.firebaseapp.com",
+  projectId: "test-3dseos",
+  storageBucket: "test-3dseos.appspot.com",
+  messagingSenderId: "630695809920",
+  appId: "1:630695809920:web:fd1d54e14aa9602ae6a765"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
